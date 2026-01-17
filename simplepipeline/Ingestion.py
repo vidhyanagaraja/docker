d6272664a2cbd1a28ea0bbeb25a25625e52f2ec9
vidhyanagaraja <vidhya.ngr@gmail.com>
2026-01-17 02:35:06 +0000

    Ingestion

---
+++ simplepipeline/Ingestion.py
@@ -0,0 +1,72 @@
+
+
+import pandas as pd
+from tqdm.auto import tqdm
+from sqlalchemy import create_engine
+
+# Read a sample of the data
+prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+# df = pd.read_csv(prefix + 'yellow_tripdata_2021-01.csv.gz', nrows=100)
+
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+
+
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+
+df_iter = pd.read_csv(
+    prefix + 'yellow_tripdata_2021-01.csv.gz',
+    dtype=dtype,
+    parse_dates=parse_dates,
+    iterator=True,
+    chunksize=100000
+)
+
+
+first = True
+
+for df_chunk in tqdm(df_iter):
+
+    if first:
+        # Create table schema (no data)
+        df_chunk.head(0).to_sql(
+            name="yellow_taxi_data",
+            con=engine,
+            if_exists="replace"
+        )
+        first = False
+        print("Table created")
+
+    # Insert chunk
+    df_chunk.to_sql(
+        name="yellow_taxi_data",
+        con=engine,
+        if_exists="append"
+    )
+
+    print("Inserted:", len(df_chunk))
+
